90ddccf9f309665d6f79334943247d189a41ee74
GabrielX <97820374+gaburielu@users.noreply.github.com>
2023-12-04 19:36:28 +0800

    finalizing operational project

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 const gameBoard = (function () {
   const gridItems = document.querySelectorAll(".grid-item");
   const resetButton = document.getElementById("reset");
+  const PlayerVsPlayerButton = document.getElementById("playeVplayer");
+  const PlayerVsComputerButton = document.getElementById("playerVcomputer");
+  const title = document.getElementById("title");
+
   let ticTacToeBoard = Array(9).fill("");
   let round = 0;
   let roundIsActive = true;
+  let isPlayerVsComputer = false;
 
-  function playRound(){
+  /////////////////////////////////////////////////
+
+  PlayerVsComputerButton.addEventListener("click", function () {
+    if (isPlayerVsComputer === false) {
+      isPlayerVsComputer = true;
+      resetBoard();
+    }
+  });
+
+  PlayerVsPlayerButton.addEventListener("click", function () {
+    if (isPlayerVsComputer === true) {
+      isPlayerVsComputer = false;
+      resetBoard();
+    }
+  });
+
+  resetButton.addEventListener("click", resetBoard);
+
+  /////////////////////////////////////////////////
+
+  function playRound() {
     gridItems.forEach((item, index) => {
-        item.addEventListener("click", () => {
-          addMark(index);
-        });
+      item.addEventListener("mouseover", () => {
+        if (roundIsActive && ticTacToeBoard[index] === "") {
+          item.textContent = getCurrentPlayer();
+          item.style.opacity = 0.5;
+        }
       });
-    resetButton.addEventListener("click", resetBoard);
+
+      item.addEventListener("mouseout", () => {
+        item.textContent = ticTacToeBoard[index];
+        item.style.opacity = 1;
+      });
+      item.addEventListener("click", () => {
+        addMark(index);
+      });
+    });
   }
 
   function addMark(index) {
@@ -25,8 +60,9 @@ const gameBoard = (function () {
     }
   }
 
-
   function resetBoard() {
+    resetButton.textContent = "Restart";
+    title.textContent = "Tic Tac Toe";
     ticTacToeBoard = Array(9).fill("");
     round = 0;
     roundIsActive = true;
@@ -34,18 +70,15 @@ const gameBoard = (function () {
   }
 
   function render() {
-    gridItems.forEach((item, index) => {
-      item.textContent = ticTacToeBoard[index];
-    });
+    buttonGameModeDisplay();
+    playerMarkerDisplay();
+    computerMove();
   }
 
   function lockBoard() {
+    resetButton.textContent = "Play again?";
     roundIsActive = false;
   }
-
-//   function returnBoard() {
-//     return ticTacToeBoard.slice();
-//   }
 
   function getCurrentPlayer() {
     if (round % 2 === 0) {
@@ -55,7 +88,51 @@ const gameBoard = (function () {
     }
   }
 
-  function drawBoard(event) {}
+  function computerPlay() {
+    const emptySpots = ticTacToeBoard.reduce(
+      (acc, val, index) => (val === "" ? acc.concat(index) : acc),
+      []
+    );
+    const randomIndex =
+      emptySpots[Math.floor(Math.random() * emptySpots.length)];
+    addMark(randomIndex);
+  }
+
+  function computerMove() {
+    if (isPlayerVsComputer) {
+      if (getCurrentPlayer() === "O") {
+        computerPlay();
+      }
+    }
+  }
+
+  function playerMarkerDisplay() {
+    gridItems.forEach((item, index) => {
+      item.textContent = ticTacToeBoard[index];
+      item.style.opacity = 1;
+      if (!roundIsActive || ticTacToeBoard[index] !== getCurrentPlayer()) {
+        item.textContent = ticTacToeBoard[index];
+      }
+
+      if (item.textContent === "X") {
+        item.style.color = "lightskyblue";
+      } else if (item.textContent === "O") {
+        item.style.color = "lightpink";
+      } else {
+        item.style.color = "";
+      }
+    });
+  }
+
+  function buttonGameModeDisplay() {
+    if (isPlayerVsComputer === true) {
+      PlayerVsComputerButton.classList.add("active");
+      PlayerVsPlayerButton.classList.remove("active");
+    } else {
+      PlayerVsComputerButton.classList.remove("active");
+      PlayerVsPlayerButton.classList.add("active");
+    }
+  }
 
   function checkCondition() {
     if (
@@ -85,16 +162,29 @@ const gameBoard = (function () {
         ticTacToeBoard[4] != "")
     ) {
       console.log(getCurrentPlayer() + " Wins the game!");
+      if (isPlayerVsComputer) {
+        if (getCurrentPlayer() === "O") {
+          title.textContent = `Computer Wins the game!`;
+        } else {
+          title.textContent = `Player Wins the game!`;
+        }
+      } else {
+        title.textContent = `${getCurrentPlayer()} Wins the game!`;
+      }
       lockBoard();
+    } else {
+      if (round === 8) {
+        title.textContent = `It's a Draw!!`;
+        lockBoard();
+      }
     }
   }
 
   playRound();
 
-  return {
-    addMark: addMark,
-    // returnBoard: returnBoard,
-    resetBoard: resetBoard,
-    render: render,
-  };
+  // return {
+  //   addMark: addMark,
+  //   resetBoard: resetBoard,
+  //   render: render,
+  // };
 })();
